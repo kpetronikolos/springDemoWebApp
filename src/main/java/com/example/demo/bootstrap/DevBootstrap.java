f591eccaab84309f2b1c899831c59adf00db5640
konstantinos <kpetronikolos@gmail.com>
2018-04-21 16:32:33 +0300

    Initialize data.

---
+++ src/main/java/com/example/demo/bootstrap/DevBootstrap.java
@@ -0,0 +1,44 @@
+package com.example.demo.bootstrap;
+
+import com.example.demo.model.Author;
+import com.example.demo.model.Book;
+import com.example.demo.repositories.AuthorRepository;
+import com.example.demo.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData() {
+        Author author1 = new Author("Eric", "Pap");
+        Book book1 = new Book("Book1", "1234", "Publisher1");
+        author1.getBooks().add(book1);
+        book1.getAuthors().add(author1);
+
+        authorRepository.save(author1);
+        bookRepository.save(book1);
+
+        Author author2 = new Author("John", "Rod");
+        Book book2 = new Book("Book2", "3421", "Publisher2");
+        author2.getBooks().add(book2);
+
+        authorRepository.save(author2);
+        bookRepository.save(book2);
+
+    }
+}
